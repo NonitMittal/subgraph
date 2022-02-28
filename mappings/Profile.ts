2ee2bcc0c39b8fba58db024cce950d4c73487fa2
Raghav Goyal <12301raghavgoyal@gmail.com>
2022-02-28 21:21:59 +0530

    add Profile event handlers

---
+++ mappings/Profile.ts
@@ -0,0 +1,51 @@
+import {ContributorProfileUpdated, ReaderProfileUpdated} from "../generated/Profile/Profile";
+import {ContributorProfile, Handle, ReaderProfile} from "../generated/schema";
+import {JSONValue, json, ipfs} from "@graphprotocol/graph-ts";
+
+export function handleContributorProfileUpdated(event: ContributorProfileUpdated): void {
+    let contributor = ContributorProfile.load(event.params.contributorAddress.toString());
+    if (contributor) {
+        let ipfsData = ipfs.cat(event.params.profileMetadataUri.toString());
+        if (ipfsData) {
+            let jsonData = json.fromBytes(ipfsData);
+            let object = jsonData.toObject();
+            if (object) {
+                contributor.username = object.get("username").toString();
+                contributor.name = object.get("name").toString();
+                contributor.username = object.get("username").toString();
+                contributor.aboutMe = object.get("aboutMe").toString();
+                contributor.handles = object
+                    .get("handles")
+                    .toArray()
+                    .map<string>((handle: JSONValue): string => {
+                        return handle.toString();
+                    });
+            }
+        }
+        contributor.save();
+    }
+}
+
+export function handleReaderProfileUpdated(event: ReaderProfileUpdated): void {
+    let reader = ReaderProfile.load(event.params.readerAddress.toString());
+    if (reader) {
+        let ipfsData = ipfs.cat(event.params.profileMetadataUri.toString());
+        if (ipfsData) {
+            let jsonData = json.fromBytes(ipfsData);
+            let object = jsonData.toObject();
+            if (object) {
+                reader.username = object.get("username").toString();
+                reader.name = object.get("name").toString();
+                reader.username = object.get("username").toString();
+                reader.aboutMe = object.get("aboutMe").toString();
+                reader.handles = object
+                    .get("handles")
+                    .toArray()
+                    .map<string>((handle: JSONValue): string => {
+                        return handle.toString();
+                    });
+            }
+        }
+        reader.save();
+    }
+}
