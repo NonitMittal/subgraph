c5a43b3ca8343598ae1bea6b7a1fd1fbf61b2314
Raghav Goyal <12301raghavgoyal@gmail.com>
2022-03-15 19:56:44 +0530

    modify RentRecord mechanism - RentRecord once set cannot be null again. - It will be created on BookPutOnRent. - It will be reinitialized each time a rentee returns the book.

--- mappings/Rentor.ts
+++ mappings/Rentor.ts
@@ -1,4 +1,4 @@
-import {BigInt} from "@graphprotocol/graph-ts";
+import {BigInt, log, store} from "@graphprotocol/graph-ts";
 import {
     BookReturned,
     BookPutOnRent,
@@ -10,8 +10,19 @@ import {RentRecord, Copy} from "../generated/schema";
 export function handleBookPutOnRent(event: BookPutOnRent): void {
     let copy = Copy.load(event.params.copyUid.toString() + event.params.editionAddress.toHex());
     if (copy) {
+        const rentRecordId =
+            event.params.copyUid.toString() +
+            event.params.editionAddress.toHex() +
+            event.block.timestamp.toString();
+        const rentRecord = new RentRecord(rentRecordId);
+        rentRecord.copyUid = event.params.copyUid;
+        rentRecord.edition = event.params.editionAddress.toHex();
+        rentRecord.flowRate = event.params.flowRate;
+        rentRecord.rentedFrom = copy.owner;
+        rentRecord.save();
         copy.onRent = true;
         copy.flowRate = event.params.flowRate;
+        copy.rentRecord = rentRecordId;
         copy.save();
     }
 }
@@ -19,32 +30,28 @@ export function handleBookPutOnRent(event: BookPutOnRent): void {
 export function handleBookRemoveFromRent(event: BookRemovedFromRent): void {
     let copy = Copy.load(event.params.copyUid.toString() + event.params.editionAddress.toHex());
     if (copy) {
-        copy.onRent = false;
-        copy.flowRate = BigInt.fromString("0");
-        copy.save();
+        let rentRecord = RentRecord.load(copy.rentRecord!);
+        if (rentRecord) {
+            if (!rentRecord.rentedTo) {
+                store.remove("RentRecord", copy.rentRecord!);
+            }
+            copy.onRent = false;
+            copy.flowRate = BigInt.fromString("0");
+            copy.rentRecord = null;
+            copy.save;
+        }
     }
 }
 
 export function handleBookTakenOnRent(event: BookTakenOnRent): void {
-    const rentRecordId =
-        event.params.copyUid.toString() +
-        event.params.editionAddress.toHex() +
-        event.block.timestamp.toString();
     let copy = Copy.load(event.params.copyUid.toString() + event.params.editionAddress.toHex());
     if (copy) {
-        let rentRecord = RentRecord.load(rentRecordId);
-        if (!rentRecord) {
-            rentRecord = new RentRecord(rentRecordId);
-            rentRecord.copyId = event.params.copyUid;
-            rentRecord.edition = event.params.editionAddress.toHex();
-            rentRecord.flowRate = event.params.flowRate;
+        const rentRecord = RentRecord.load(copy.rentRecord!);
+        if (rentRecord) {
             rentRecord.rentedTo = event.params.rentedTo.toHex();
-            rentRecord.rentedFrom = copy.owner;
             rentRecord.rentStartDate = event.block.timestamp;
             rentRecord.save();
         }
-        copy.rentRecord = rentRecordId;
-        copy.save();
     }
 }
 
@@ -55,7 +62,17 @@ export function handleBookReturned(event: BookReturned): void {
         if (rentRecord) {
             rentRecord.rentEndDate = event.block.timestamp;
             rentRecord.save();
-            copy.rentRecord = null;
+            const rentRecordId =
+                event.params.copyUid.toString() +
+                event.params.editionAddress.toHex() +
+                event.block.timestamp.toString();
+            rentRecord = new RentRecord(rentRecordId);
+            rentRecord.copyUid = event.params.copyUid;
+            rentRecord.edition = event.params.editionAddress.toHex();
+            rentRecord.flowRate = copy.flowRate;
+            rentRecord.rentedFrom = copy.owner;
+            rentRecord.save();
+            copy.rentRecord = rentRecordId;
             copy.save;
         }
     }
